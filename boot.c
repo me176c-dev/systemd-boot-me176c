d11d88b9ff6d30e7ba38fffa5d6956b1a9352d91
lambdadroid <lambdadroid@gmail.com>
2017-12-27 17:33:46 +0100

    Use UP key to select a boot option
    The power button isn't reported as a key in the UEFI environment.
    Use the (Volume) Up button as a workaround so an option can be
    selected.

--- boot.c
+++ boot.c
@@ -685,7 +685,6 @@ static BOOLEAN menu_run(
                 idx_highlight_prev = idx_highlight;
 
                 switch (key) {
-                case KEYPRESS(0, SCAN_UP, 0):
                 case KEYPRESS(0, 0, 'k'):
                         if (idx_highlight > 0)
                                 idx_highlight--;
@@ -734,6 +733,7 @@ static BOOLEAN menu_run(
                                 idx_highlight = config->entry_count-1;
                         break;
 
+                case KEYPRESS(0, SCAN_UP, 0):
                 case KEYPRESS(0, 0, CHAR_LINEFEED):
                 case KEYPRESS(0, 0, CHAR_CARRIAGE_RETURN):
                         exit = TRUE;
