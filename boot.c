ebed826ae22e156cfb3222192cf9f49e1a8c0c29
lambdadroid <lambdadroid@gmail.com>
2017-12-29 16:39:56 +0100

    Initialize config->idx_default_efivar to -1 earlier
    With LoaderEntryOneShot set, config->idx_default_efivar would be
    set to 0, even if the variable is not present. Initialize it earlier
    to avoid that.

--- boot.c
+++ boot.c
@@ -1256,6 +1256,8 @@ static VOID config_default_entry_select(Config *config) {
         EFI_STATUS err;
         UINTN i;
 
+        config->idx_default_efivar = -1;
+
         /*
          * The EFI variable to specify a boot entry for the next, and only the
          * next reboot. The variable is always cleared directly after it is read.
@@ -1298,7 +1300,6 @@ static VOID config_default_entry_select(Config *config) {
                                 return;
                         }
         }
-        config->idx_default_efivar = -1;
 
         if (config->entry_count == 0)
                 return;
